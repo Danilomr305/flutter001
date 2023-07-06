48a5ee340a6289c708922d5884331ed35fa7781b
Danilomr305 <danilomr305@gmail.com>
2023-07-06 11:56:29 -0300

    g

--- aula_001/lib/main.dart
+++ aula_001/lib/main.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: unused_local_variable, prefer_typing_uninitialized_variables
+
 import 'package:aula_001/classes/boletim.dart';
 
 main() {
-  var boletim = Boletim(aluno: aluno, disciplinas: disciplinas)
+  var aluno;
+  var disciplinas;
+  var boletim = Boletim(aluno: aluno, disciplinas: disciplinas);
 }
