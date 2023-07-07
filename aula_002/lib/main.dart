1af439f5db0aa63e55879a8ab59e1a19453a6203
Danilomr305 <danilomr305@gmail.com>
2023-07-06 21:47:27 -0300

    s

--- aula_002/lib/main.dart
+++ aula_002/lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp (const 
+  runApp ( 
     MaterialApp(
-      home: Scaffold(appBar: AppBar(),), 
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(backgroundColor: Colors.blueGrey[900],
+        title: const Text('Eu Sou Rico'),
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+              'https://s2-techtudo.glbimg.com/1o2J-rf2G9qtlQlm82gaq-mFBec=/0x129:1024x952/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/7/i/ME2AxRRoygUyFPCDe0jQ/3.png'
+            ),
+          ),
+        ),
+      ),
     ),
-  );
+  );    
 }
