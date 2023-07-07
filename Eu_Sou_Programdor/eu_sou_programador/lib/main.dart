0187df9c82a4afe4dfc13bb4857f4ef55249184e
Danilomr305 <danilomr305@gmail.com>
2023-07-07 00:25:48 -0300

    a

--- Eu_Sou_Programdor/eu_sou_programador/lib/main.dart
+++ Eu_Sou_Programdor/eu_sou_programador/lib/main.dart
@@ -4,22 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
-
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
+at it has a State object (defined below) that contains fields that affect
   // how it looks.
 
   // This class is the configuration for the state. It holds the values (in this
