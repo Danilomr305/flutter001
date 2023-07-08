c11abb43b757543a8e68324c63e312d03fa849b4
Danilomr305 <danilomr305@gmail.com>
2023-07-08 11:55:28 -0300

    f

--- app_my_card/lib/main.dart
+++ app_my_card/lib/main.dart
@@ -2,12 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.deepOrange,
-        body: Container()
-      ),
-    ),
+    const MeuApp(),
   );
 }
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepOrange,
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            margin: const EdgeInsets.only(left:30.0),
+            height: 100.0,
+            width: 100.0,
+            color: Colors.white,
+            child: const Text('Olá Pessoal!'),
+          ),
+        )
+      ),
+    );
+  }
+}
+
+
 
