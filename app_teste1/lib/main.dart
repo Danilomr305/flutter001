b9eb2de16251844e55fc661f7ba85c746400d535
Danilomr305 <danilomr305@gmail.com>
2023-07-09 18:02:51 -0300

    d

--- app_teste1/lib/main.dart
+++ app_teste1/lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( 
-    MaterialApp(
-      home: back,
-    )
+  runApp(
+    const MeuApp(),
   );
 }
 
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
 
- 
-
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold
+      (
+      backgroundColor: Colors.indigoAccent,
+      child: Text('Eu Sou Programador'), 
+      ),
+    );
+  }
+}
