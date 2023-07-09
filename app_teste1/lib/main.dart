39b8feccd0153b58fcf54e9f1948fb2830e31128
Danilomr305 <danilomr305@gmail.com>
2023-07-09 18:46:54 -0300

    h

--- app_teste1/lib/main.dart
+++ app_teste1/lib/main.dart
@@ -2,8 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
    runApp(
-    MeuApp( )
+    const MeuApp( )
   );
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black54,
+      
+    );
+  }
 }
 
 
