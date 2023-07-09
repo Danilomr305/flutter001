5e4ea9a7b2d781cac64a1bae365896a89319bd42
Danilomr305 <danilomr305@gmail.com>
2023-07-09 13:19:42 -0300

    k

--- app_teste1/lib/main.dart
+++ app_teste1/lib/main.dart
@@ -7,15 +7,6 @@ void main() {
 
  
 
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
