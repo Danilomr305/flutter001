fb13e14f5239adc7aa2b679f613ba41a1bce4c2c
Danilomr305 <danilomr305@gmail.com>
2023-07-09 18:51:22 -0300

    i

---
+++ desafios/app_test/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MeuApp()
+  );
+}
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
