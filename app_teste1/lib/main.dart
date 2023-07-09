9ef8682adc8dc9c32d72899996fd357851ef5b43
Danilomr305 <danilomr305@gmail.com>
2023-07-09 13:18:46 -0300

    m

--- app_teste1/lib/main.dart
+++ app_teste1/lib/main.dart
@@ -4,17 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a blue toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
