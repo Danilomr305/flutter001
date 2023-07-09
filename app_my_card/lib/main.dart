113bfae72d87347165413c73572b26421e337b92
Danilomr305 <danilomr305@gmail.com>
2023-07-09 13:01:15 -0300

    s

--- app_my_card/lib/main.dart
+++ app_my_card/lib/main.dart
@@ -15,14 +15,29 @@ class MeuApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
         body: SafeArea(
-          child: Container(
-            padding: const EdgeInsets.all(20.0),
-            margin: const EdgeInsets.only(left:30.0),
-            height: 100.0,
-            width: 100.0,
-            color: Colors.white,
-            child: const Text('Olá Pessoal!'),
-          ),
+          child: Column(
+            Verti
+            children: <Widget>[
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.white,
+              child: const Text('Container 1'),
+             ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.blue,
+              child: const Text('Container 2')
+            ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.red,
+              child: const Text('Container 3')
+            ),
+           ],
+          )
         )
       ),
     );
