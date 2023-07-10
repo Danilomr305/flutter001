5039c51acd35694f41cd4ae7bccdba38c44eb88d
Danilomr305 <danilomr305@gmail.com>
2023-07-10 12:07:56 -0300

    s

--- desafios/app_test/lib/main.dart
+++ desafios/app_test/lib/main.dart
@@ -13,16 +13,36 @@ class MeuApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.amberAccent,
-       body: SafeArea(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              height: 17.0,
-              width: 50.0,
-              color: Colors.grey,
+        body: SafeArea(
+          child:  Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 1000.0,
+                width: 90.0,
+                color: Colors.grey,
+                child: const Text('Alo voce'),
+                ),
+              Container(
+                height: 1000.0,
+                width: 90.0,
+                color: Colors.blue,
+                child: const Text('Alo vocee'),
               ),
-            ],
+              Container(
+                height: 900.0,
+                width: 100.0,
+                color: Colors.deepPurple,
+                child: const Text('Alo vocee'),
+              ),
+              Container(
+                height: 1000.0,
+                width: 90.0,
+                color: Colors.orange,
+                child: const Text('Alo voceee'),
+              ),
+              ],
+            ),
           ),
         ),
       ),
