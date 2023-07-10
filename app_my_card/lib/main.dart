aa3ddf33b53d093e8e00beeca523c974305ca81e
Danilomr305 <danilomr305@gmail.com>
2023-07-10 12:22:02 -0300

    x

--- app_my_card/lib/main.dart
+++ app_my_card/lib/main.dart
@@ -12,34 +12,12 @@ class MeuApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-              height: 100.0,
-              color: Colors.white,
-              child: const Text('Container 1'),
-             ),
-             const SizedBox(
-              width: 20.0
-              ),
-              Container(
-                height: 100.0,
-                color: Colors.blue,
-                child: const Text('Container 2')
-            ),
-              Container(
-                height: 100.0,
-                color: Colors.red,
-                child: const Text('Container 3')
-            ),
-           ],
-          ),
-        )
+          child: Column(children: <Widget>[CircleAvatar()],),
+        ),
       ),
     );
   }
