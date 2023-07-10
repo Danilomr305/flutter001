7ac8aff05c317e9a32067149bcc5af72844637a1
Danilomr305 <danilomr305@gmail.com>
2023-07-10 12:15:06 -0300

    j

--- desafios/app_test/lib/main.dart
+++ desafios/app_test/lib/main.dart
@@ -5,47 +5,45 @@ void main() {
     const MeuApp()
   );
 }
+
 class MeuApp extends StatelessWidget {
   const MeuApp({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amberAccent,
+        backgroundColor: Colors.deepOrange,
         body: SafeArea(
-          child:  Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Container(
-                height: 1000.0,
-                width: 90.0,
-                color: Colors.grey,
-                child: const Text('Alo voce'),
-                ),
+              height: 100.0,
+              color: Colors.white,
+              child: const Text('Container 1'),
+             ),
+             const SizedBox(
+              width: 20.0
+              ),
               Container(
-                height: 1000.0,
-                width: 90.0,
+                height: 100.0,
                 color: Colors.blue,
-                child: const Text('Alo vocee'),
-              ),
-              Container(
-                height: 900.0,
-                width: 100.0,
-                color: Colors.deepPurple,
-                child: const Text('Alo vocee'),
-              ),
-              Container(
-                height: 1000.0,
-                width: 90.0,
-                color: Colors.orange,
-                child: const Text('Alo voceee'),
-              ),
-              ],
+                child: const Text('Container 2')
             ),
+              Container(
+                height: 100.0,
+                color: Colors.red,
+                child: const Text('Container 3')
+            ),
+           ],
           ),
-        ),
+        )
       ),
     );
   }
 }
+
+
+
