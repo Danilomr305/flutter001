37a6068a898403081ce0dbe20c0c075d4da8f05d
Danilomr305 <danilomr305@gmail.com>
2023-07-12 23:24:57 -0300

    h

--- app_my_card/lib/main.dart
+++ app_my_card/lib/main.dart
@@ -5,7 +5,7 @@ void main() {
      MeuApp(),
   );
 }
-// ignore: use_key_in_widget_constructors
+
 class MeuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,53 +14,32 @@ class MeuApp extends StatelessWidget {
         backgroundColor: Colors.deepOrange,
         body: SafeArea(
           child: Column(
-            children: <Widget>[
-            CircleAvatar(
-              backgroundImage: AssetImage('imagens/perfil.jpg'),
-              radius: 50.0,
+              children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('imagens/perfil.jpg'),
+                radius: 50.0,
+                ),
+                Text(
+                  'Danilo Martins', 
+                  style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico'
+                ),
               ),
               Text(
-                'Danilo Martins', 
+                'DESENVOLVEDOR fLUTTER',
                 style: TextStyle(
-                fontSize: 40.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Pacifico'
-              ),
-            ),
-            Text(
-              'DESENVOLVEDOR fLUTTER',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-                letterSpacing: 2.5, 
-                fontWeight: FontWeight.bold,
-              ), 
-            ), 
-           
-            Row(
-              textDirection: TextDirection.ltr,
-              children: <Widget>[
-                Icon(
-                  Icons.phone,
-                  size: 35.0,
                   color: Colors.white,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    '+55 1234 5678',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Pacifico',
-                      fontSize: 25.0,
-                    ),
-                ),
+                  fontSize: 20.0,
+                  letterSpacing: 2.5, 
+                  fontWeight: FontWeight.bold,
+                ), 
+              ), 
+              
               ],
-            )
-            ],
-          ),
+            ),
         ),
       ),
     );
