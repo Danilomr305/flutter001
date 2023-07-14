f1f50dd7938eb1f1efe896356dedb456bae56f88
Danilomr305 <danilomr305@gmail.com>
2023-07-14 00:21:28 -0300

    h

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -18,21 +18,26 @@ class Calculadora extends StatelessWidget {
           backgroundColor: Colors.blueGrey,
           title:const Text('Calculadora'),
         ),
-        body: SafeArea(child:
-         Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.white,
-              child: const Text('Visor'),
-            ),
-            ],
-          ),
-        ),
+           Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 150.0,
+                width: 409.0,
+                color: Colors.white,
+                padding: const EdgeInsets.all(60.0),
+                child: const Text('VISOR',
+                textAlign: 
+                TextAlign.center,
+                style: 
+                TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w900),
+                  ),  
+              ),
+              ],
+           ),
       ),
     );
   }
 }
-
