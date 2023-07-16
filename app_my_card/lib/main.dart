324a77a0ecbe76462980e5055b382d3d6eac0538
Danilomr305 <danilomr305@gmail.com>
2023-07-16 18:13:59 -0300

    s

--- app_my_card/lib/main.dart
+++ app_my_card/lib/main.dart
@@ -8,13 +8,20 @@ void main() {
 
   class MeuApp extends StatelessWidget {
   const MeuApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            margin: const EdgeInsets.all(20.0),
+            height: 100.0,
+            width: 100.0,
+            color: Colors.brown,
+            child: const Text('Olá Pessoal!!'),
+          ),
+        ),
       ),
     );
   }
