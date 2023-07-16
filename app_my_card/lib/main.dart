2a3717bbe28d3283cbf65213e4f07d534d92702d
Danilomr305 <danilomr305@gmail.com>
2023-07-16 12:26:11 -0300

    ds

--- app_my_card/lib/main.dart
+++ app_my_card/lib/main.dart
@@ -2,16 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-     const MeuApp(),
+      const MeuApp(),
   );
 }
 
-class MeuApp extends StatelessWidget {
+
+
+
+
+  class MeuApp extends StatelessWidget {
   const MeuApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepOrange,
+        body: Container(),
+      ),
+    );
   }
 }
   
