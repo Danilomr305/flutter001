27fe20d2a3b01b39e984b3c45ea14138503d64e6
Danilomr305 <danilomr305@gmail.com>
2023-07-20 14:33:47 -0300

    r

--- app_jogo_dado/lib/main.dart
+++ app_jogo_dado/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart' show AppBar, BuildContext, Colors, Container, MaterialApp, Scaffold, StatelessWidget, Text, Widget, runApp;
+import 'package:flutter/material.dart';
 
 void main() {
   runApp( 
--- jogo_dados/android/app/src/main/kotlin/com/example/jogo_dados/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jogo_dados
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
