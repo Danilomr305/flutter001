f34f5931d6affffdc8524dc0eaff11a52b3fef49
Danilomr305 <danilomr305@gmail.com>
2023-07-19 22:07:23 -0300

    d

---
+++ app_jogo_dado/android/app/src/main/kotlin/com/example/app_jogo_dado/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_jogo_dado
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_jogo_dado/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( 
+    const MeuApp()
+  );
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
