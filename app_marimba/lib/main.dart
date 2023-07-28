914cb25e1c1cc11d7f90a7329475abe6b94f6c26
Danilomr305 <danilomr305@gmail.com>
2023-07-28 13:50:10 -0300

    t

--- app_marimba/lib/main.dart
+++ app_marimba/lib/main.dart
@@ -8,7 +8,7 @@ void main() => runApp(const MarimbaApp());
 
 class MarimbaApp extends StatelessWidget { 
   const MarimbaApp({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
