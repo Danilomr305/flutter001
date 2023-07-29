82a5dc4191a0919b78af62befdc9cfa0aa2dc175
Danilomr305 <danilomr305@gmail.com>
2023-07-29 15:56:23 -0300

    cc

--- app_marimba/lib/main.dart
+++ app_marimba/lib/main.dart
@@ -10,7 +10,7 @@ class MarimbaApp extends StatelessWidget {
   @override 
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold( 
+      home: Scaffold(  
         body: SafeArea(
           child: Center( 
             child: TextButton(
