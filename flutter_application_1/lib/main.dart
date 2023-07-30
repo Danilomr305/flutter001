37f109b7580e90e82ea8f69d35cdd58f14b5a507
Danilomr305 <danilomr305@gmail.com>
2023-07-30 19:09:26 -0300

    q

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -13,10 +13,90 @@ class Quizzler extends StatelessWidget {
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: Quizzler(),
+            child: QuizPage(),
           ),
         ),
       ),
     );
   }
 }
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        const Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'As perguntas serão exibidas aqui.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              onPressed: () {}, 
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'Verdadeiro',
+                       style: TextStyle(
+                       fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ), 
+            ),
+          ),
+        ), 
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              onPressed: () {}, 
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'Falso',
+                       style: TextStyle(
+                       fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ), 
+            ),
+          ),
+        ), 
+      ],
+    );
+  }
+}
+
+
