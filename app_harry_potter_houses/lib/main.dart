525da5133e21283d8a9d668e652c31e7b29bf4b0
Danilomr305 <danilomr305@gmail.com>
2023-08-05 00:35:17 -0300

    x

--- app_harry_potter_houses/lib/main.dart
+++ app_harry_potter_houses/lib/main.dart
@@ -1,6 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const Harry());
+
+class Harry extends StatelessWidget {
+  const Harry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const HarryPoterPage(),
+    );
+  } 
+}
+
+class HarryPoterPage extends StatefulWidget {
+  const HarryPoterPage({super.key});
+
+  @override
+  State<HarryPoterPage> createState() => _HarryPoterPageState();
+}
+
+class _HarryPoterPageState extends State<HarryPoterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // ignore: avoid_unnecessary_containers
+      body: Container(
+        padding: 
+          const EdgeInsets.
+            symmetric(
+              vertical: 50.0,
+              horizontal: 50.0),
+        constraints: const BoxConstraints.expand(),
+        child: const Image(
+          image: AssetImage(
+            'imagens/'
+          ),
+        ),
+      ),
+    );
+  }
 }
 
