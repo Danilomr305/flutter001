83ab2a230fedb47d85f31103e794f13855752582
Danilomr305 <danilomr305@gmail.com>
2023-08-06 00:24:44 -0300

    3

--- app_harry_potter_houses/lib/main.dart
+++ app_harry_potter_houses/lib/main.dart
@@ -9,13 +9,14 @@ class Harry extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: const HarryPoterPage(),
+      home: const Harry(),
     );
+    
   } 
 }
 
 class HarryPoterPage extends StatefulWidget {
-  const HarryPoterPage({super.key});
+  const HarryPoterPage({super.key, required AppBar appBar});
 
   @override
   State<HarryPoterPage> createState() => _HarryPoterPageState();
@@ -26,6 +27,15 @@ class _HarryPoterPageState extends State<HarryPoterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       // ignore: avoid_unnecessary_containers
+      appBar: AppBar(
+        title: const Text(
+          'HARRY POTTER',
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+      ),
       body: Container(
        decoration: const BoxDecoration(
         image: DecorationImage(
@@ -41,7 +51,15 @@ class _HarryPoterPageState extends State<HarryPoterPage> {
         child: const SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[],
+            children: <Widget>[
+              Expanded(
+                flex: 12, 
+                child: Center(
+                  child: 
+                  Padding(padding: EdgeInsets.only(top: 120.0)),
+                ),
+              ),
+            ],
           ),
         ),
       ),
