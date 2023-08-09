048a21e0921a0ec5a5e5be27058dc58f333f0177
Danilomr305 <danilomr305@gmail.com>
2023-08-09 14:30:21 -0300

    r

--- app_marimba/lib/main.dart
+++ app_marimba/lib/main.dart
@@ -3,26 +3,3 @@ import 'package:flutter/material.dart';
 
 // ignore: prefer_const_constructors
 void main() => runApp(MarimbaApp());
-
-class MarimbaApp extends StatelessWidget { 
-  const MarimbaApp({super.key});
- 
-  @override 
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(  
-        body: SafeArea(
-          child: Center( 
-            child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(UrlSource('toque/nota1.wav'));
-              }, 
-              child:  const Text('Clique em mim!'),
-            ),
-          ), 
-        ),
-      ), 
-    );
-  }
-}
