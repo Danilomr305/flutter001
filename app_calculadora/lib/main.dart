83411fb4d029330f8223c75897f9fc3592be740a
Danilomr305 <danilomr305@gmail.com>
2023-08-10 22:52:51 -0300

    d

--- app_calculadora/lib/main.dart
+++ app_calculadora/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_full_hex_values_for_flutter_colors
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const CalculadoraIMC());
@@ -8,16 +10,17 @@ class CalculadoraIMC extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    theme: ThemeData(
-       colorScheme: ColorScheme.fromSwatch().copyWith(
-        primary: Colors.deepOrange,
-        secondary: Colors.green,
+      theme: ThemeData(
+        primaryColor: const Color(0xFFff0000),
+        scaffoldBackgroundColor: const Color(0xFF482356),
+        ac
+        textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.white,),),
       ),
-        textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.purple)),
-    ),
-    home: const TelaPrincipal(),
+    // ignore: prefer_const_constructors
+    home: TelaPrincipal(),
     );
-  }     
+  }
+  
 }
 
 class TelaPrincipal extends StatefulWidget {
